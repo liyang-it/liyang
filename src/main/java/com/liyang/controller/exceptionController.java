1af05bc57075bb893e1b8cd81160fdf6f90d3c95
liyangit <1922802352@qq.com>
2020-11-20 11:18:46 +0800

    修改 全局异常控制器

---
+++ src/main/java/com/liyang/controller/exceptionController.java
@@ -0,0 +1,38 @@
+package com.liyang.controller;
+
+import com.liyang.json.jsonResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestControllerAdvice
+@CrossOrigin
+  public class exceptionController {
+    private Logger log = LoggerFactory.getLogger(exceptionController.class);
+
+    /**
+     * 404 异常
+     * @param e
+     * @param request
+     * @return
+     */
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    public jsonResult exceptionError404(NoHandlerFoundException e, HttpServletRequest request){
+      String  msg = "失败！找不到相关接口[{"+request.getRequestURL()+"},{"+request.getRemoteAddr()+"}]";
+      log.error(msg);
+      return jsonResult.resultError(msg);
+    }
+
+  /**
+   * 全局异常
+   */
+  @ExceptionHandler(value = Exception.class)
+  public jsonResult exceptionError(Exception e){
+    String msg = e.getMessage();
+    log.error(msg);
+    return jsonResult.resultError(msg);
+  }
+}
