6c1d91813d76832294db2788710d6975a292e0b3
liyangit <1922802352@qq.com>
2021-02-02 17:27:09 +0800

    影视数据具体类

---
+++ src/main/java/com/liyang/entity/cms/video.java
@@ -0,0 +1,120 @@
+package com.liyang.entity.cms;
+
+public class video {
+  private String name;
+  private String sub;
+  private String type;
+  private String pic;
+  private String actor;
+  private String remarks;
+  private String area;
+  private String lang;
+  private String year;
+  private String content;
+  private String playUrl;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSub() {
+    return sub;
+  }
+
+  public void setSub(String sub) {
+    this.sub = sub;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getPic() {
+    return pic;
+  }
+
+  public void setPic(String pic) {
+    this.pic = pic;
+  }
+
+  public String getActor() {
+    return actor;
+  }
+
+  public void setActor(String actor) {
+    this.actor = actor;
+  }
+
+  public String getRemarks() {
+    return remarks;
+  }
+
+  public void setRemarks(String remarks) {
+    this.remarks = remarks;
+  }
+
+  public String getArea() {
+    return area;
+  }
+
+  public void setArea(String area) {
+    this.area = area;
+  }
+
+  public String getLang() {
+    return lang;
+  }
+
+  public void setLang(String lang) {
+    this.lang = lang;
+  }
+
+  public String getYear() {
+    return year;
+  }
+
+  public void setYear(String year) {
+    this.year = year;
+  }
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public String getPlayUrl() {
+    return playUrl;
+  }
+
+  public void setPlayUrl(String playUrl) {
+    this.playUrl = playUrl;
+  }
+
+  public video(String name, String sub, String type, String pic, String actor, String remarks, String area, String lang, String year, String content, String playUrl) {
+    this.name = name;
+    this.sub = sub;
+    this.type = type;
+    this.pic = pic;
+    this.actor = actor;
+    this.remarks = remarks;
+    this.area = area;
+    this.lang = lang;
+    this.year = year;
+    this.content = content;
+    this.playUrl = playUrl;
+  }
+
+  public video() {
+  }
+}
